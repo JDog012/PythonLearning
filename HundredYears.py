2817813a3b9e3b85838914e15dd8037df87ec26b
Josh Harrington <josh1harrington@gmail.com>
2022-08-26 20:41:41 -0600

    This is actually my first python practice code.

---
+++ HundredYears.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("What is your current age? ")
+hundred_year = str(2022 - int(age) + 100)
+
+print('Hello '+ name +', you will be one hundred in the year '+ hundred_year+'.')
